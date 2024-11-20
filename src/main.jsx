294f5d3afc458484660c3aa55e0e4611881db622
Alberto Noguchi <alberto2611@gmail.com>
2024-11-20 00:59:57 -0300

    add: using useState todo list solution

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./02-Preserving_values_with_useState/01-Light_Switch/App";
+import App from "./03-Using_useState/01-Todo/TodoList";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/wtf.jsx
+++
@@ -1,7 +0,0 @@
-export default function Counter() {
-  let count = 0;
-
-  const handleClick = () => count++;
-
-  return <button onClick={handleClick}>{count}</button>;
-}
