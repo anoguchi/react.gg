9cef44adbc4d7bd4e7bfd396214866a744d307ce
Alberto Noguchi <alberto2611@gmail.com>
2024-11-21 22:41:27 -0300

    add: using state password toggle solution

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./03-Using_useState/02-Smart_Character_Limit/App";
+import App from "./03-Using_useState/03-Password_Toggle/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
