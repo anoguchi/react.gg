a6bdbcb3e1669d6e1809cb18c7d65171b1b9665a
Alberto Noguchi <alberto2611@gmail.com>
2024-11-21 22:11:46 -0300

    add: using state smart character limit solution

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./03-Using_useState/01-Todo/TodoList";
+import App from "./03-Using_useState/02-Smart_Character_Limit/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
