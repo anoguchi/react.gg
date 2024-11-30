4966ba26eebb27aa71757e31d397f09afefb1503
Alberto Noguchi <alberto2611@gmail.com>
2024-11-30 17:37:23 -0300

    add: managing effects clock

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./05-Managing_effects_pt2/App";
+import App from "./05-Managing_effects_pt2/02-Clock/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/wtf.jsx
+++ src/wtf.jsx
@@ -1,18 +1,3 @@
-import * as React from "react";
-import { fetchPokemon } from "./api";
+In this challenge, you'll be synchronizing the result of fetching country data from an external API (the url below) with your component's state.
 
-export default function App() {
-  const [id, setId] = React.useState(1);
-  const [pokemon, setPokemon] = React.useState(null);
-
-  React.useEffect(() => {
-    const handleFetchPokemon = async () => {
-      const { error, response } = await fetchPokemon(id);
-      setPokemon(response);
-    };
-
-    handleFetchPokemon();
-  }, [id]);
-
-  return <main>{JSON.stringify({ id, pokemon }, null, 2)}</main>;
-}
+The JSX is finished, so all you need to do is fetch the data from the following URL and update the component's state with the result.
