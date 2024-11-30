b11141c332d866ffc4361720c25dff8581fb8162
Alberto Noguchi <alberto2611@gmail.com>
2024-11-30 18:12:53 -0300

    add: managing effects hacker news

--- src/wtf.jsx
+++
@@ -1,3 +0,0 @@
-In this challenge, you'll be synchronizing the result of fetching country data from an external API (the url below) with your component's state.
-
-The JSX is finished, so all you need to do is fetch the data from the following URL and update the component's state with the result.
