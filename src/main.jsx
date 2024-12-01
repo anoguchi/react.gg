b5239911e6ddcd88030d08321830bd23101247fb
Alberto Noguchi <alberto2611@gmail.com>
2024-12-01 17:40:45 -0300

    add: preserving values with useRef follow the leader

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./3-Escaping_React/06-Preserving_values_with_useRef/07-Expanding_textarea/App";
+import App from "./3-Escaping_React/06-Preserving_values_with_useRef/08-Follow_the_leader/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
