112595166b9780069e4b24c41cdc8ed31c770ea1
Alberto Noguchi <alberto2611@gmail.com>
2024-12-01 17:25:34 -0300

    add: preserving values with useRef expanding textarea

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./3-Escaping_React/06-Preserving_values_with_useRef/06-Click_outside/App";
+import App from "./3-Escaping_React/06-Preserving_values_with_useRef/07-Expanding_textarea/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
