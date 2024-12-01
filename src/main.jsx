606f0abafd7436540ad17d3560d50164ec50c28e
Alberto Noguchi <alberto2611@gmail.com>
2024-12-01 17:58:33 -0300

    add: teleportation with context auth and translations

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./3-Escaping_React/06-Preserving_values_with_useRef/08-Follow_the_leader/App";
+import App from "./3-Escaping_React/07-Teleportation_with_context/03-Translations/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
