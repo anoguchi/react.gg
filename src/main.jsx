c7710a7a002b57ba971bfb98e05ba6ce8397c498
Alberto Noguchi <alberto2611@gmail.com>
2024-12-01 17:07:54 -0300

    add: preserving values with useRef click outside

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./3-Escaping_React/06-Preserving_values_with_useRef/04-Video_player/App";
+import App from "./3-Escaping_React/06-Preserving_values_with_useRef/06-Click_outside/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
