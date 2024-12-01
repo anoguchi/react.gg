c0c534a15a33f47ec3e991c69eface57b82f73e1
Alberto Noguchi <alberto2611@gmail.com>
2024-12-01 16:54:16 -0300

    add: preserving values with useRef video player

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./3-Escaping_React/06-Preserving_values_with_useRef/03-Autofocus/App";
+import App from "./3-Escaping_React/06-Preserving_values_with_useRef/04-Video_player/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
