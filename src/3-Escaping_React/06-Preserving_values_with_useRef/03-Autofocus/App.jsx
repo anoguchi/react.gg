dc00840e293d8c84bf0451383c4d5e2bb4ae5010
Alberto Noguchi <alberto2611@gmail.com>
2024-12-01 16:42:46 -0300

    add: preserving values with useRef autofocus

---
+++ src/3-Escaping_React/06-Preserving_values_with_useRef/03-Autofocus/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import * as React from "react";
+
+function TextInput() {
+  const inputRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Autofocus Input</h1>
+      <label htmlFor="focus">Email Address</label>
+      <input
+        id="focus"
+        ref={inputRef}
+        type="email"
+        placeholder="Enter your email"
+      />
+    </div>
+  );
+}
+
+export default TextInput;
