11c3b93a0a418c05e6286c29c1e064d02a4e39c3
Alberto Noguchi <alberto2611@gmail.com>
2024-12-02 20:31:09 -0300

    add: complex state with useReducer add to cart

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./4-Optimizing_React/01-Complex_state_with_useReducer/03-Task_manager/App";
+import App from "./4-Optimizing_React/01-Complex_state_with_useReducer/04-Add_to_cart/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
