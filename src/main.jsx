b1aba331d56df631ec28dc6bddf55bae87b02d87
Alberto Noguchi <alberto2611@gmail.com>
2024-12-02 20:56:35 -0300

    add: referential equality and why it matters data table

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./4-Optimizing_React/02-Referential_equality_and_why_it_matters/02-Localized_primes/App";
+import App from "./4-Optimizing_React/02-Referential_equality_and_why_it_matters/03-Data_table/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
