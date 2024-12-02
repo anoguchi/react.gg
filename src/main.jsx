4b9ffc637126271c2f3d2f10f6f5addc8e8bfa71
Alberto Noguchi <alberto2611@gmail.com>
2024-12-02 20:23:30 -0300

    add: complex state with useReducer task manager

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./4-Optimizing_React/01-Complex_state_with_useReducer/02-Multistep_form_with_useReducer/App";
+import App from "./4-Optimizing_React/01-Complex_state_with_useReducer/03-Task_manager/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
