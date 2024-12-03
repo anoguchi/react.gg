8dc682dd105ab7bfb1da12203f3ca5cb058caea8
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 01:00:55 -0300

    add: rebuilding useHooks useCounter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/08-useInterval/App";
+import App from "./6-Rebuilding_useHooks/09-useCounter/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
