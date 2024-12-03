dca9653e1d7b53dbad2252353bd3f44339d7fbf9
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 17:37:09 -0300

    add: rebuilding useHooks usedLockBodyScroll

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/09-useCounter/App";
+import App from "./6-Rebuilding_useHooks/10-useLockBodyScroll/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
