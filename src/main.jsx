6f353b8f6fa33a40ec01ca878896c8a8988f9508
Alberto Noguchi <alberto2611@gmail.com>
2024-12-02 23:27:44 -0300

    add: rebuilding useHooks useDefault

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/01-useDocumentTitle/App";
+import App from "./6-Rebuilding_useHooks/02-useDefault/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
