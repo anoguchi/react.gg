9acfe4695b16b3a72f9350713ea25a856458724a
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 00:00:46 -0300

    add: rebuilding useHooks usePreferredLanguage

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/04-usePrevious/App";
+import App from "./6-Rebuilding_useHooks/05-usePreferredLanguage/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
