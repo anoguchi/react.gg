86407d9063c60e3dc63df9ac5b24207a21aa95ff
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 00:21:29 -0300

    add: rebuilding useHooks useCopyToClipboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/06-useFavicon/App";
+import App from "./6-Rebuilding_useHooks/07-useCopyToClipboard/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
