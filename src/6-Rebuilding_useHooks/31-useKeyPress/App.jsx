c9f606d4f38ac12db29e70309479690425eb576b
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 22:46:02 -0300

    add: rebuilding useKeyPress

--- src/6-Rebuilding_useHooks/31-useKeyPress/App.jsx
+++ src/6-Rebuilding_useHooks/31-useKeyPress/App.jsx
@@ -1,31 +1,42 @@
 import "./App.css";
-import useOrientation from "./useOrientation";
+import * as React from "react";
+import useKeyPress from "./useKeyPress";
 
 export default function App() {
-  const orientation = useOrientation();
+  const [activeKey, setActiveKey] = React.useState("");
+
+  const handleKeyPress = (e) => {
+    e.preventDefault();
+    setActiveKey(e.key);
+    setTimeout(() => {
+      setActiveKey("");
+    }, 600);
+  };
+
+  useKeyPress("ArrowRight", handleKeyPress);
+  useKeyPress("ArrowLeft", handleKeyPress);
+  useKeyPress("ArrowUp", handleKeyPress);
+  useKeyPress("ArrowDown", handleKeyPress);
 
   return (
     <section>
-      <h1>useOrientation</h1>
-
-      <article
-        style={{ "--angle": `${orientation.angle}deg` }}
-        className={orientation.type.toLocaleLowerCase()}
-      />
-      <div>
-        <table>
-          <tbody>
-            {Object.keys(orientation).map((key) => {
-              return (
-                <tr key={key}>
-                  <th>{key}</th>
-                  <td>{orientation[key]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <h1>useKeyPress</h1>
+      <p>Press one of the arrow keys on your keyboard</p>
+      <article>
+        <button className={activeKey === "ArrowUp" ? "pressed" : ""}>
+          <span>&uarr;</span>
+        </button>
+        <button className={activeKey === "ArrowLeft" ? "pressed" : ""}>
+          <span>&larr;</span>
+        </button>
+        <button className={activeKey === "ArrowDown" ? "pressed" : ""}>
+          <span>&darr;</span>
+        </button>
+        <button className={activeKey === "ArrowRight" ? "pressed" : ""}>
+          <span>&rarr;</span>
+        </button>
+      </article>
+      {Boolean(activeKey) && <label>{activeKey} was pressed</label>}
     </section>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/30-usePageLeave/App";
+import App from "./6-Rebuilding_useHooks/31-useKeyPress/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
