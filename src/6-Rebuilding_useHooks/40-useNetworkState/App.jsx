1b08a34ee26a2cfd2bad2c6f0f093f7e6b51ca2d
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 23:23:21 -0300

    add: rebuilding useNetworkState

--- src/6-Rebuilding_useHooks/40-useNetworkState/App.jsx
+++ src/6-Rebuilding_useHooks/40-useNetworkState/App.jsx
@@ -1,31 +1,25 @@
 import "./App.css";
-import useOrientation from "./useOrientation";
+import useNetworkState from "./useNetworkState";
 
 export default function App() {
-  const orientation = useOrientation();
+  const network = useNetworkState();
 
   return (
     <section>
-      <h1>useOrientation</h1>
+      <h1>useNetworkState</h1>
 
-      <article
-        style={{ "--angle": `${orientation.angle}deg` }}
-        className={orientation.type.toLocaleLowerCase()}
-      />
-      <div>
-        <table>
-          <tbody>
-            {Object.keys(orientation).map((key) => {
-              return (
-                <tr key={key}>
-                  <th>{key}</th>
-                  <td>{orientation[key]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <table>
+        <tbody>
+          {Object.keys(network).map((key) => {
+            return (
+              <tr key={key} className={key}>
+                <th>{key}</th>
+                <td>{`${network[key]}`}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </section>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/39-useIsClient/App";
+import App from "./6-Rebuilding_useHooks/40-useNetworkState/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
