2cc4aad97613d80194ca3fadfe8c0201aae5b374
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 22:02:18 -0300

    add: rebuilding useOrientation

--- src/6-Rebuilding_useHooks/28-useOrientation/App.jsx
+++ src/6-Rebuilding_useHooks/28-useOrientation/App.jsx
@@ -1,61 +1,31 @@
 import "./App.css";
-import Form from "./Form";
-import useHistoryState from "./useHistoryState";
+import useOrientation from "./useOrientation";
 
 export default function App() {
-  const { state, set, undo, redo, clear, canUndo, canRedo } = useHistoryState({
-    items: [],
-  });
-
-  const addTodo = (val) => {
-    set({
-      ...state,
-      items: state.items.concat({ id: crypto.randomUUID(), name: val }),
-    });
-  };
-
-  const removeTodo = (id) => {
-    set({
-      ...state,
-      items: state.items.filter((item) => item.id !== id),
-    });
-  };
+  const orientation = useOrientation();
 
   return (
     <section>
-      <header>
-        <h1>useHistoryState</h1>
-        <div>
-          <button disabled={!canUndo} className="link" onClick={undo}>
-            Undo
-          </button>
-          <button disabled={!canRedo} className="link" onClick={redo}>
-            Redo
-          </button>
+      <h1>useOrientation</h1>
 
-          <button
-            disabled={!state.items.length}
-            className="link"
-            onClick={clear}
-          >
-            Clear
-          </button>
-        </div>
-        <Form addItem={addTodo} />
-      </header>
-
-      <ul>
-        {state.items.map((item, index) => {
-          return (
-            <li key={index}>
-              <span>{item.name}</span>
-              <button className="link" onClick={() => removeTodo(item.id)}>
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <article
+        style={{ "--angle": `${orientation.angle}deg` }}
+        className={orientation.type.toLocaleLowerCase()}
+      />
+      <div>
+        <table>
+          <tbody>
+            {Object.keys(orientation).map((key) => {
+              return (
+                <tr key={key}>
+                  <th>{key}</th>
+                  <td>{orientation[key]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/27-useLogger/App";
+import App from "./6-Rebuilding_useHooks/28-useOrientation/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
