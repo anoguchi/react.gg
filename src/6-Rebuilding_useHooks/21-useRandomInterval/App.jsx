4a722e9e3e31d2a870a6730be28cdb0ee289e869
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 21:18:05 -0300

    add: rebuilding useRandomInterval

--- src/6-Rebuilding_useHooks/21-useRandomInterval/App.jsx
+++ src/6-Rebuilding_useHooks/21-useRandomInterval/App.jsx
@@ -1,61 +1,27 @@
 import "./App.css";
-import Form from "./Form";
-import useHistoryState from "./useHistoryState";
+import * as React from "react";
+import useRandomInterval from "./useRandomInterval";
+import HeartsDemo from "./Heart";
 
 export default function App() {
-  const { state, set, undo, redo, clear, canUndo, canRedo } = useHistoryState({
-    items: [],
-  });
+  const demo = React.useRef(new HeartsDemo());
+  const clear = useRandomInterval(
+    () => {
+      demo.current.addHeart();
+    },
+    { minDelay: 50, maxDelay: 3000 }
+  );
 
-  const addTodo = (val) => {
-    set({
-      ...state,
-      items: state.items.concat({ id: crypto.randomUUID(), name: val }),
-    });
-  };
-
-  const removeTodo = (id) => {
-    set({
-      ...state,
-      items: state.items.filter((item) => item.id !== id),
-    });
-  };
+  React.useEffect(() => {
+    demo.current.loop();
+  }, []);
 
   return (
     <section>
-      <header>
-        <h1>useHistoryState</h1>
-        <div>
-          <button disabled={!canUndo} className="link" onClick={undo}>
-            Undo
-          </button>
-          <button disabled={!canRedo} className="link" onClick={redo}>
-            Redo
-          </button>
-
-          <button
-            disabled={!state.items.length}
-            className="link"
-            onClick={clear}
-          >
-            Clear
-          </button>
-        </div>
-        <Form addItem={addTodo} />
-      </header>
-
-      <ul>
-        {state.items.map((item, index) => {
-          return (
-            <li key={index}>
-              <span>{item.name}</span>
-              <button className="link" onClick={() => removeTodo(item.id)}>
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <h1>useRandomInterval</h1>
+      <button className="link" onClick={clear}>
+        Stop
+      </button>
     </section>
   );
 }
