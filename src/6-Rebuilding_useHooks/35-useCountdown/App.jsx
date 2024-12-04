c71c666cdccd1489eedeb3646972b65d48b17e39
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 22:58:53 -0300

    add: rebuilding useCountdown

--- src/6-Rebuilding_useHooks/35-useCountdown/App.jsx
+++ src/6-Rebuilding_useHooks/35-useCountdown/App.jsx
@@ -1,31 +1,36 @@
 import "./App.css";
-import useOrientation from "./useOrientation";
+import * as React from "react";
+import useCountdown from "./useCountdown";
 
 export default function App() {
-  const orientation = useOrientation();
+  const [endTime, setEndTime] = React.useState(new Date(Date.now() + 10000));
+  const [complete, setComplete] = React.useState(false);
+
+  const count = useCountdown(endTime, {
+    interval: 1000,
+    onTick: () => console.log("tick"),
+    onComplete: (time) => setComplete(true),
+  });
+
+  const handleClick = (time) => {
+    if (complete === true) return;
+    const nextTime = endTime.getTime() + time;
+    setEndTime(new Date(nextTime));
+  };
 
   return (
     <section>
-      <h1>useOrientation</h1>
-
-      <article
-        style={{ "--angle": `${orientation.angle}deg` }}
-        className={orientation.type.toLocaleLowerCase()}
-      />
-      <div>
-        <table>
-          <tbody>
-            {Object.keys(orientation).map((key) => {
-              return (
-                <tr key={key}>
-                  <th>{key}</th>
-                  <td>{orientation[key]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <header>
+        <h1>useCountdown</h1>
+      </header>
+      <span className="countdown">{count}</span>
+      {complete === false && (
+        <div className="button-row">
+          <button onClick={() => handleClick(5000)}>+5s</button>
+          <button onClick={() => handleClick(10000)}>+10s</button>
+          <button onClick={() => handleClick(15000)}>+15s</button>
+        </div>
+      )}
     </section>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/34-useFetch/App";
+import App from "./6-Rebuilding_useHooks/35-useCountdown/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
