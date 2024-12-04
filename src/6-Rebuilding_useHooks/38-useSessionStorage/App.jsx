62b3d12c368c90f618f6c82a40efe676d293b23c
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 23:19:38 -0300

    add: rebuilding useSessionStorage

--- src/6-Rebuilding_useHooks/38-useSessionStorage/App.jsx
+++ src/6-Rebuilding_useHooks/38-useSessionStorage/App.jsx
@@ -1,31 +1,50 @@
 import "./App.css";
-import useOrientation from "./useOrientation";
+import useSessionStorage from "./useSessionStorage";
+import { cart } from "./icons";
 
 export default function App() {
-  const orientation = useOrientation();
+  const [count, setCount] = useSessionStorage("woot", 0);
 
   return (
     <section>
-      <h1>useOrientation</h1>
+      <h1>useSessionStorage</h1>
 
-      <article
-        style={{ "--angle": `${orientation.angle}deg` }}
-        className={orientation.type.toLocaleLowerCase()}
-      />
       <div>
-        <table>
-          <tbody>
-            {Object.keys(orientation).map((key) => {
-              return (
-                <tr key={key}>
-                  <th>{key}</th>
-                  <td>{orientation[key]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <button className="link" onClick={() => setCount(0)}>
+          Clear Cart
+        </button>
+        <button
+          className="link"
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Reload Window
+        </button>
+        <button
+          className="link"
+          onClick={() => {
+            window.sessionStorage.clear();
+            window.location.reload();
+          }}
+        >
+          Clear Session
+        </button>
       </div>
+
+      <button className="primary" onClick={() => setCount(count + 1)}>
+        <span>{cart}</span> Add To Cart
+      </button>
+
+      <button className="cart">
+        <span>{cart}</span>{" "}
+        <span
+          key={count}
+          className={`cart-count ${count > 0 ? "animate" : ""}`}
+        >
+          {count}
+        </span>
+      </button>
     </section>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./6-Rebuilding_useHooks/37-useLocalStorage/App";
+import App from "./6-Rebuilding_useHooks/38-useSessionStorage/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
