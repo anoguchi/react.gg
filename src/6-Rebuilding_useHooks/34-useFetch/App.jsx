852efd2745c6c2a344136ea349c4dcb94ba95ed8
Alberto Noguchi <alberto2611@gmail.com>
2024-12-03 22:56:48 -0300

    add: rebuilding useFetch

--- src/6-Rebuilding_useHooks/34-useFetch/App.jsx
+++ src/6-Rebuilding_useHooks/34-useFetch/App.jsx
@@ -1,31 +1,29 @@
 import "./App.css";
-import useOrientation from "./useOrientation";
+import * as React from "react";
+import useFetch from "./useFetch";
+import Card from "./Card";
 
 export default function App() {
-  const orientation = useOrientation();
+  const [count, setCount] = React.useState(1);
+
+  const { error, data } = useFetch(
+    `https://pokeapi.co/api/v2/pokemon/${count}`
+  );
 
   return (
     <section>
-      <h1>useOrientation</h1>
-
-      <article
-        style={{ "--angle": `${orientation.angle}deg` }}
-        className={orientation.type.toLocaleLowerCase()}
-      />
-      <div>
-        <table>
-          <tbody>
-            {Object.keys(orientation).map((key) => {
-              return (
-                <tr key={key}>
-                  <th>{key}</th>
-                  <td>{orientation[key]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <h1>useFetch</h1>
+      <button
+        disabled={count < 2}
+        className="link"
+        onClick={() => setCount((c) => c - 1)}
+      >
+        Prev
+      </button>
+      <button className="link" onClick={() => setCount((c) => c + 1)}>
+        Next
+      </button>
+      <Card loading={!data} error={error} data={data} />
     </section>
   );
 }
